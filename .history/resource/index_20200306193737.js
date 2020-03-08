d4a4e83ba13782ae2d22b5e414940c6c2992b1ae
aplaaypung29 <729346015@qq.com>
2020-03-08 15:55:02 +0800

    1

---
+++ .history/resource/index_20200306193737.js
@@ -0,0 +1,55 @@
+const headerEl = document.querySelector('header')
+
+window.addEventListener('scroll', () => {
+  let height = headerEl.getBoundingClientRect().height
+  // let height = headerEl.offsetHeight
+  if (window.pageYOffset - height > 800) {
+    if (!headerEl.classList.contains('sticky')) {
+      headerEl.classList.add('sticky')
+    }
+  } else {
+    headerEl.classList.remove('sticky')
+  }
+})
+
+// 轮播图
+const glide = new Glide('.glide')
+const captionEl = document.querySelectorAll('.slide-caption')
+
+glide.on(['mount.after', 'run.after'], () => {
+  const caption = captionEl[glide.index]
+  anime({
+    targets: caption.children,
+    opacity: [0, 1],
+    duration: 400,
+    easing: 'linear',
+    delay: anime.stagger(400, { start: 300 }),
+    translateY: [anime.stagger([40, 10]), 0]
+  })
+})
+
+glide.on('run.before', () => {
+  document.querySelectorAll('.slide-caption > *').forEach(el => {
+    el.style.opacity = 0
+  })
+})
+
+glide.mount()
+
+const isotope = new Isotope('.cases', {
+  layoutMode: 'fitRows',
+  itemSelector: '.case-item'
+})
+// 分类按钮
+const filterBtns = document.querySelector('.filter-btns')
+filterBtns.addEventListener('click', e => {
+  let { target } = e
+  const filterOption = target.getAttribute('data-filter')
+  if (filterOption) {
+    document
+      .querySelectorAll('.filter-btn.active')
+      .forEach(btn => btn.classList.remove('active'))
+    target.classList.add('active')
+    isotope.arrange({ filter: filterOption })
+  }
+})
---
+++ .history/resource/index_20200306194249.js
@@ -0,0 +1,63 @@
+const headerEl = document.querySelector('header')
+const scrollToTop = document.querySelector('.scrollToTop')
+window.addEventListener('scroll', () => {
+  let height = headerEl.getBoundingClientRect().height
+  // 顶部导航栏会出现
+  if (window.pageYOffset - height > 800) {
+    if (!headerEl.classList.contains('sticky')) {
+      headerEl.classList.add('sticky')
+    }
+  } else {
+    headerEl.classList.remove('sticky')
+  }
+  // 返回顶部按钮出现
+  if (window.pageYOffset > 2000) {
+    scrollToTop.style.display = 'block'
+  } else {
+    scrollToTop.style.display = 'none'
+  }
+})
+// 返回顶部
+scrollToTop.addEventListener
+
+// 轮播图
+const glide = new Glide('.glide')
+const captionEl = document.querySelectorAll('.slide-caption')
+
+glide.on(['mount.after', 'run.after'], () => {
+  const caption = captionEl[glide.index]
+  anime({
+    targets: caption.children,
+    opacity: [0, 1],
+    duration: 400,
+    easing: 'linear',
+    delay: anime.stagger(400, { start: 300 }),
+    translateY: [anime.stagger([40, 10]), 0]
+  })
+})
+
+glide.on('run.before', () => {
+  document.querySelectorAll('.slide-caption > *').forEach(el => {
+    el.style.opacity = 0
+  })
+})
+
+glide.mount()
+
+const isotope = new Isotope('.cases', {
+  layoutMode: 'fitRows',
+  itemSelector: '.case-item'
+})
+// 分类按钮
+const filterBtns = document.querySelector('.filter-btns')
+filterBtns.addEventListener('click', e => {
+  let { target } = e
+  const filterOption = target.getAttribute('data-filter')
+  if (filterOption) {
+    document
+      .querySelectorAll('.filter-btn.active')
+      .forEach(btn => btn.classList.remove('active'))
+    target.classList.add('active')
+    isotope.arrange({ filter: filterOption })
+  }
+})
